b46a944d8d0c07793302ca6a270661e78fee6e58
pujabam <cubixtech69@gmail.com>
2022-12-21 08:23:46 +0545

    check

--- header.js
+++ header.js
@@ -48,7 +48,7 @@ class Myheader extends HTMLElement {
               </li>
 
               <li class="nav-item">
-                <a class="nav-link" href="#">Projects</a>
+                <a class="nav-link" href="project.html">Projects</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">Teams</a>
